38f6c9685642df63a69dfe2705c072be316a8274
Hariom <hariomchouhan430@gmail.com>
2025-02-23 11:06:16 +0530

    In Home page HomeCard is show the real values like Total balance, Total Income, Total Expenses

--- components/HomeCard.tsx
+++ components/HomeCard.tsx
@@ -5,10 +5,35 @@ import { colors, spacingX, spacingY } from "@/constants/theme";
 import { scale, verticalScale } from "@/utils/styling";
 import { ArrowDown, ArrowUp, DotsThreeOutline } from "phosphor-react-native";
 import { currentCurrency } from "@/constants/currency";
+import useFetchData from "@/hooks/useFetchData";
+import { WalletType } from "@/types";
+import { useAuth } from "@/contexts/authContext";
+import { orderBy, where } from "firebase/firestore";
 
 type Props = {};
 
 const HomeCard = (props: Props) => {
+  const { user } = useAuth();
+  const {
+    data: wallets,
+    loading: walletLoading,
+    error,
+  } = useFetchData<WalletType>("wallets", [
+    where("uid", "==", user?.uid),
+    orderBy("created", "desc"),
+  ]);
+
+  const getTotals = () => {
+    return wallets.reduce(
+      (totals: any, item: WalletType) => {
+        totals.balance = totals.balance + Number(item.amount);
+        totals.income = totals.income + Number(item.totalIncome);
+        totals.expenses = totals.expenses + Number(item.totalExpenses);
+        return totals;
+      },
+      { balance: 0, income: 0, expenses: 0 }
+    );
+  };
   return (
     <ImageBackground
       source={require("@/assets/images/card.png")}
@@ -29,7 +54,7 @@ const HomeCard = (props: Props) => {
             />
           </View>
           <Typo color={colors.black} size={30} fontWeight={"bold"}>
-            {currentCurrency} 1,000.00
+            {currentCurrency} {walletLoading ? "----" : getTotals()?.balance?.toFixed(2)}
           </Typo>
         </View>
 
@@ -51,7 +76,7 @@ const HomeCard = (props: Props) => {
             </View>
             <View style={{ alignSelf: "center" }}>
               <Typo size={17} color={colors.green} fontWeight={"600"}>
-                {currentCurrency} 2345
+                {currentCurrency} {walletLoading ? "----" : getTotals()?.income?.toFixed(2)}
               </Typo>
             </View>
           </View>
@@ -71,7 +96,7 @@ const HomeCard = (props: Props) => {
             </View>
             <View style={{ alignSelf: "center" }}>
               <Typo size={17} color={colors.rose} fontWeight={"600"}>
-                {currentCurrency} 234432
+                {currentCurrency} {walletLoading ? "----" : getTotals()?.expenses?.toFixed(2)}
               </Typo>
             </View>
           </View>
